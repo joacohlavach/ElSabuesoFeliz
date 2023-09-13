1ad893045ba3050d62685554438d0873b8ffa8cf
fran-monti123 <francismontini@gmail.com>
2023-09-13 18:32:24 -0300

    models

--- app/models.py
+++ app/models.py
@@ -8,3 +8,39 @@ class Veterinaria(models.Model):
     def __str__(self):
         return self.name
     
+
+class Perro(models.Model):
+    numeroHistoriaClinica = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    fechaNacimiento = models.DateField()
+    raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
+    pesoActual = models.FloatField()
+    alturaActual = models.FloatField()
+    duenio = models.ForeignKey(Duenio, on_delete=models.CASCADE)
+    consulta = models.TextField()
+    vacuna = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Raza(models.Model):
+    denominacion = models.CharField(max_length=100)
+    pesoMinimoMachos = models.FloatField()
+    pesoMaximoMachos = models.FloatField()
+    pesoMinimoHembras = models.FloatField()
+    pesoMaximoHembras = models.FloatField()
+    alturaMediaMachos = models.FloatField()
+    alturaMediaHembras = models.FloatField()
+    cuidadosEspeciales = models.TextField()
+
+    def __str__(self):
+        return self.denominacion
+    
+class Duenio(models.Model):
+    apellido = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombres}"
+    
