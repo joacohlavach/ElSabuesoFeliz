a381287763686d03a57841344d3ca2b9b2302594
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2023-09-15 15:49:46 -0300

    Templates

--- app/static/JS/index.js
+++ app/static/JS/index.js
@@ -1,1 +1,7 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
