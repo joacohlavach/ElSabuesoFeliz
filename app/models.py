fd165d949370cb8a69d0631324a89cf31f0504aa
fran-monti123 <francismontini@gmail.com>
2023-09-17 14:13:23 -0300

    models class

--- app/models.py
+++ app/models.py
@@ -1,9 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class Sucursal(models.Model):
+    direccion = models.CharField(max_length=255)
     
+    def __str__(self):
+        return self.direccion
+
+class Empleado(models.Model):
+    numeroDocumento = models.IntegerField()
+    nombres = models.CharField(max_length=255)
+    apellido = models.CharField(max_length=255)
+    fechaNacimiento = models.DateField()
+    fechaIngreso = models.DateField()
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellido}"
+
+class AsignacionEmpleados(models.Model):
+    fechaIngreso = models.DateField()
+    fechaEgreso = models.DateField()
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Perro(models.Model):
+    numeroHistoriaClinica = models.IntegerField()
+    nombre = models.CharField(max_length=255)
+    fechaNacimiento = models.DateField()
+    raza = models.ForeignKey('Raza', on_delete=models.CASCADE)
+    pesoActual = models.FloatField()
+    alturaActual = models.FloatField()
+    historialMascotas = models.ForeignKey('HistorialMascotas', on_delete=models.CASCADE)
+    consulta = models.TextField()
+    vacuna = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class HistorialMascotas(models.Model):
+    estadoPerro = models.CharField(max_length=255)
+    rolPersona = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.estadoPerro
+
+class Persona(models.Model):
+    nombrePersona = models.CharField(max_length=255)
+    numDocPersona = models.IntegerField()
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombrePersona
+
 class Raza(models.Model):
-    denominacion = models.CharField(max_length=100)
+    denominacion = models.CharField(max_length=255)
     pesoMinimoMachos = models.FloatField()
     pesoMaximoMachos = models.FloatField()
     pesoMinimoHembras = models.FloatField()
@@ -14,27 +66,67 @@ class Raza(models.Model):
 
     def __str__(self):
         return self.denominacion
-    
-class Duenio(models.Model):
-    apellido = models.CharField(max_length=100)
-    nombres = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=15)
+
+class Consulta(models.Model):
+    numeroOrden = models.IntegerField()
+    fechaEntrada = models.DateField()
+    sIntomas = models.TextField()
+    diagnosticos = models.TextField()
+    medicamento = models.TextField()
+    fechaSalida = models.DateField()
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    perro = models.ForeignKey(Perro, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.apellido}, {self.nombres}"
-class Perro(models.Model):
-    numeroHistoriaClinica = models.IntegerField()
-    nombre = models.CharField(max_length=100)
-    fechaNacimiento = models.DateField()
-    raza = models.ForeignKey(Raza, on_delete=models.CASCADE)
-    pesoActual = models.FloatField()
-    alturaActual = models.FloatField()
-    duenio = models.ForeignKey(Duenio, on_delete=models.CASCADE)
-    consulta = models.TextField()
-    vacuna = models.CharField(max_length=100)
+        return f"Consulta para {self.perro.nombre}"
+
+class Vacunacion(models.Model):
+    fechaProgramada = models.DateField()
+    fechaReal = models.DateField()
+    descripcion = models.TextField()
 
     def __str__(self):
-        return self.name
-    
+        return self.descripcion
 
-    
+class Vacuna(models.Model):
+    nombreVacuna = models.CharField(max_length=255)
+    empleado = models.CharField(max_length=255)
+    laboratorio = models.CharField(max_length=255)
+    dosis = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombreVacuna
+
+class Medicamento(models.Model):
+    nombre = models.CharField(max_length=255)
+    laboratorio = models.CharField(max_length=255)
+    fechaUltimaCompra = models.DateField()
+    cantidadExistente = models.IntegerField()
+    cantidadMinima = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class DuenioPerro(models.Model):
+    apellido = models.CharField(max_length=255)
+    nombres = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellido}"
+
+class EstadoEmpleado(models.Model):
+    estudiante = models.CharField(max_length=255)
+    recibido = models.CharField(max_length=255)
+    trabaja = models.CharField(max_length=255)
+    noTrabaja = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.estudiante
+
+class RolEmpleado(models.Model):
+    supervisor = models.CharField(max_length=255)
+    supervisorSuplente = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.supervisor
