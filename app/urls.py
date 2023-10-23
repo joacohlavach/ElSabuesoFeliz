1e2bfd17d2cc158f82bf1b28dda421e3f7d582a4
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2023-10-23 08:24:27 -0300

    .

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .views import *
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
     path('', base, name = 'main'),
     path('login/', login, name = 'loginPerro'),
     path('razas/', razaperro, name='Raza'),
+
+    path('inicio/', views.inicio_view, name='inicio'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
+    
--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+@login_required
 
 def base(request):
     return render(request,'Veterinaria_list.html')
@@ -10,5 +16,17 @@ def login(request):
 def razaperro(request):
     return render(request, 'razaperro_template.html')
 
-def hola (request):
-    ...
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
