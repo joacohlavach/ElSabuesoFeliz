5817e44735a41719b9b6548a29d56764ea36df99
fran-monti123 <francismontini@gmail.com>
2023-10-23 08:08:08 -0300

    registro

--- app/views.py
+++ app/views.py
@@ -5,10 +5,15 @@ def base(request):
     return render(request,'Veterinaria_list.html')
     
 def login(request):
-    return render(request, 'LoginPerros.html')
+    if request.method == 'GET':
+        return render(request, 'LoginPerros.html')
+    
+    else:
+        p = Perro.objects.create()
 
 def razaperro(request):
     return render(request, 'razaperro_template.html')
 
 def hola (request):
     ...
+
